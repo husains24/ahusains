90c4e723ed405456529184e783fda62c951f442a
husains24 <husainsr24@gmail.com>
2025-11-09 19:31:12 +0530

    code changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ahusains/',
   plugins: [react()],
-  base: './ahusains/',
-  server: {
-    port: 3000
-  }
-});
+})
